9f00ad94a1d22b928646c9dbba5c8e5105e5c5e2
Chikaia Dev <30716319+ChikaiaDev@users.noreply.github.com>
2019-08-10 14:38:08 +0300

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="d-flex align-items-center app-header ">
         <div class="p-2">
-            <a class="navbar-brand" href="#">KodeKlinik</a>
+            <a class="navbar-brand" href="#">Kode-Klinik</a>
         </div>
         
         <ul class="nav app-nav">
