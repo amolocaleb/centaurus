1491929327a517c6c3f8a71daf4bf22747df1697
collinsochieng <collinsochiengopondo@gmail.com>
2019-08-24 02:34:29 +0300

    be cool

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -91,14 +91,14 @@
                         <div class="col-md-6 mb-5 mb-lg-0 position-relative">
                             <img src="images/about.jpg" class="img-fluid" alt="Image">
                             <div class="experience">
-                                <span class="year">50 years</span>
+                                <span class="year">10 years</span>
                                 <span class="caption">of experience</span>
                             </div>
 
                         </div>
                         <div class="col-md-5 ml-auto">
                             <h3 class="section-sub-title">About Us</h3>
-                            <h2 class="section-title mb-3">Welcome To Consula</h2>
+                            <h2 class="section-title mb-3">Welcome To KodeKlinik</h2>
                             <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui fuga ipsa, repellat blanditiis nihil, consectetur. Consequuntur eum inventore, rem maxime, nisi excepturi ipsam libero ratione adipisci alias eius vero vel!</p>
                             <p><a href="#" class="btn btn-primary btn-black--hover">Learn More</a></p>
                         </div>
