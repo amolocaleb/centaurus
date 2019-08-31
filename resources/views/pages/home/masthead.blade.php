7effc1687e3a6b1b9032f471ec3e807b780383f4
Caleb <amolocaleb@gmail.com>
2019-08-31 16:07:36 +0300

    Some changes

--- resources/views/pages/home/masthead.blade.php
+++ resources/views/pages/home/masthead.blade.php
@@ -5,17 +5,13 @@
             <div class="col-12 text-center">
                 <h1 class="font-weight-light">Vertically Centered Masthead Content</h1>
                 <p class="lead">A great starter layout for a landing page</p>
-                <button class="btn app-btn">Hire Our Developers<i class="fa fa-play-circle-o"></i></button>
+                <button class="btn app-btn">Hire Our Developers</button>
+                <button class="play-btn"><i class="fa fa-play-circle play-icon"></i></button>
             </div>
         </div>
     </div>
 </header>
 
-{{-- <div class="kodeklinik-box">
-    <div class="content">
-       <p class="h2">Hire On Demand Developers</p> 
-    </div>
-    
-</div>  --}}
+
 
 
